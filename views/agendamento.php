6b018ebb650b7a96cb72dc1c8dadb8be9469aece
Rafickszzz <joaoraficcesjo@gmail.com>
2023-03-30 15:21:31 -0300

    ajuste no agendamento

---
+++ views/agendamento.php
@@ -0,0 +1,288 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/templates/cabecalho.php";
+?>
+
+<!-- conteudo principal aqui -->
+<div class="conteiner">
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            08:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            08:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            09:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            09:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            09:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            10:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            10:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            11:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            11:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            12:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            12:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            13:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            14:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            14:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            15:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            15:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            16:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            16:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            17:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            17:30</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+
+    <div id="hora-teste">
+        <button class="accordion">
+            <span class="material-symbols-outlined" type="button">
+                add_circle
+            </span>
+            18:00</button>
+        <div class="panel">
+            <?php
+            require $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/form.php";
+            ?>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<!--Calendario-->
+<?php 
+require_once $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/php/calendario.php";
+?>
+
+<!-- chamar os js aqui embaixo -->
+<script src="/hipocrates/js/sapatenis.js"></script>
+
+<?php 
+require_once $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/templates/rodape.php";
+?>
