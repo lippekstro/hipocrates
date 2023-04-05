c9c90d7017396022bd5eb2def03c47e07f1b703e
DaronBecaus <danielsilvasoder@gmail.com>
2023-04-05 10:46:02 -0300

    Optimização de código

--- js/script.js
+++ js/script.js
@@ -13,11 +13,10 @@ sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
 
+const items = document.querySelectorAll(".item");
 document.getElementById("next").onclick = function () {
-  let lists = document.querySelectorAll(".item");
-  document.getElementById("slide").appendChild(lists[0]);
+  document.getElementById("slide").appendChild(items[0]);
 };
 document.getElementById("prev").onclick = function () {
-  let lists = document.querySelectorAll(".item");
-  document.getElementById("slide").prepend(lists[lists.length - 1]);
+  document.getElementById("slide").prepend(items[items.length - 1]);
 };
