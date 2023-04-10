da579ea16886a291cebb8eda955d4c99b524702e
Dnmaster-Dev <estevaothatymartins2325@gmail.com>
2023-04-10 20:15:17 -0300

    fizemos pagina sobre

---
+++ views/sobre.php
@@ -0,0 +1,47 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/templates/cabecalho.php";
+?>
+
+
+<div class="tit1">
+
+    <p>Nova História</p>
+
+</div>
+
+<div class="text1">
+
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi error corrupti, dolor neque doloribus
+        necessitatibus saepe harum consequatur nulla. Nostrum voluptatum saepe, excepturi asperiores sequi minima
+        tenetur accusantium in temporibus.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sit dignissimos ullam facilis eius
+        accusamus natus, suscipit provident exercitationem consequatur molestiae maiores facere sequi tenetur harum
+        labore quia doloribus soluta!
+
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, aspernatur ipsa, voluptate quis temporibus,
+        dolor laboriosam at officiis molestiae eligendi voluptates minus optio rem provident earum aperiam minima
+        repudiandae alias.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, mollitia. Veniam quia non ipsum eum vero
+        unde sequi nemo expedita rerum ullam velit inventore, voluptatem excepturi adipisci sunt harum reiciendis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti rerum sequi, quo iure exercitationem
+        repellendus nisi earum dolores omnis inventore deserunt vitae enim. Dolor aperiam, qui laborum ducimus eos
+        rerum.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis sed porro blanditiis iure quod aliquam
+        ducimus eveniet veniam, non explicabo nobis, expedita nesciunt tenetur et sint quos! Obcaecati, nobis iusto.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptate consequuntur quidem eum facere, aut
+        voluptatibus dolor expedita laboriosam assumenda molestias incidunt possimus aliquid. Cupiditate pariatur
+        nobis nesciunt non error.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, earum? Beatae, ducimus alias eligendi omnis
+        quidem harum! Nam optio itaque cumque, quia delectus quos molestias. Expedita maiores doloribus consectetur
+        quae?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id eos magni enim, quia esse voluptatem nam
+        architecto eum ducimus unde suscipit dolore, ab dolores tempora delectus itaque cupiditate? Cum, quo.
+    </p>
+
+</div>
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/templates/rodape.php";
+?>
