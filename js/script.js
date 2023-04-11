22037a17a97bc5082984d256b34d242eec4e4a7b
Daniel Sodré <126109661+DaronBecaus@users.noreply.github.com>
2023-04-11 09:54:59 -0300

    Voltei o que era antes JS
    Lo deje como estap, porque por alguna razón desconocida el codigo anterior no permitia que hubiera el cambio de pagina adequado.

--- js/script.js
+++ js/script.js
@@ -13,10 +13,11 @@ sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
 
-const items = document.querySelectorAll(".item");
 document.getElementById("next").onclick = function () {
-  document.getElementById("slide").appendChild(items[0]);
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").appendChild(lists[0]);
 };
 document.getElementById("prev").onclick = function () {
-  document.getElementById("slide").prepend(items[items.length - 1]);
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").prepend(lists[lists.length - 1]);
 };
