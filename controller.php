381dfa37fb5c6ef8906e819cf0446dd5ab2992c7
DaronBecaus <danielsilvasoder@gmail.com>
2023-04-14 15:22:25 -0300

    add responsavel

--- controller.php
+++ controller.php
@@ -1,6 +1,7 @@
 <?php
 require_once "paciente.php";
 require_once "endereco.php";
+require_once "responsavel.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -64,5 +65,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $endereco->inserir();
     } catch (Exception $e) {
         echo "Erro ao inserir endereço: " . $e->getMessage();
+    }
+
+    try {
+        $cpf = $_POST['cpf'];
+        $nome_mae = $_POST['nome_mae'];
+        $nome_pai = $_POST['nome_pai'];
+        $nome_conjuge = $_POST['nome_conjuge'];
+        $nome_responsavel = $_POST['nome_responsavel'];
+
+        $responsavel = new responsavel();
+
+        $responsavel->cpf = $cpf;
+        $responsavel->nome_mae = $nome_mae;
+        $responsavel->nome_pai = $nome_pai;
+        $responsavel->nome_conjuge = $nome_conjuge;
+        $responsavel->nome_responsavel = $nome_responsavel;
+        $responsavel->inserir();
+    } catch (Exception $e) {
+        echo "Erro ao inserir responsavel: " . $e->getMessage();
     }
 }
