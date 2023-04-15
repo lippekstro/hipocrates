1ce7cff31ba97704cfd3101e6af9f18814108d04
Daniel Sodré <126109661+DaronBecaus@users.noreply.github.com>
2023-04-15 20:26:15 -0300

    Update agendamento.php

--- agendamento.php
+++ agendamento.php
@@ -10,7 +10,7 @@ date_default_timezone_set('America/Sao_Paulo');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>document</title>
+    <title>Agendamento</title>
 </head>
 
 <body>
