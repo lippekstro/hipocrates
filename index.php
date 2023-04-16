3dab4660264880771d68149a2492af055948db24
DaronBecaus <danielsilvasoder@gmail.com>
2023-04-16 20:18:15 -0300

    Imagem mais leve

--- index.php
+++ index.php
@@ -12,35 +12,35 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/hipocrates/models/cabecalho.php"
           <a class="button-page" href="#">Mais Informações</a>
         </div>
       </div>
-      <div class="item" style="background-image: url(imgs/2.jpg);">
+      <div class="item" style="background-image: url(imgs/1.jpg);">
         <div class="content">
           <div class="name">Agentamentos</div>
           <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas iste assumenda sit officia itaque corporis? Vero fugiat iure ut qui? Iusto consequuntur dolores quaerat accusamus voluptas voluptates maiores, alias iure?</div>
           <a class="button-page" href="#">Mais Informações</a>
         </div>
       </div>
-      <div class="item" style="background-image: url(imgs/3.jpg);">
+      <div class="item" style="background-image: url(imgs/1.jpg);">
         <div class="content">
           <div class="name">Sobre Nós</div>
           <div class="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum velit fugit facilis aut explicabo facere. Ut nostrum provident texterunt minima recusandae qui modi. Ad voluptatibus sunt eum voluptas error. Laudantium?</div>
           <a class="button-page" href="#">Mais Informações</a>
         </div>
       </div>
-      <div class="item" style="background-image: url(imgs/4.jpg);">
+      <div class="item" style="background-image: url(imgs/1.jpg);">
         <div class="content">
           <div class="name">Noticias</div>
           <div class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione itaque ipsam facere ipsum aut expedita, maiores repellendus atque, consequatur dolor ad, sed fugiat harum dolore aliquam obcaecati aliquid a saepe?</div>
           <a class="button-page" href="#">Mais Informações</a>
         </div>
       </div>
-      <div class="item" style="background-image: url(imgs/5.jpg);">
+      <div class="item" style="background-image: url(imgs/1.jpg);">
         <div class="content">
           <div class="name">Fale Conosco</div>
           <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, quod quaerat aperiam non corrupti quidem. Nostrum quia, cupiditate quas quidem ipsam aspernatur. Itaque ullam voluptatem incidunt inventore assumenda possimus a!</div>
           <a class="button-page" href="#">Mais Informações</a>
         </div>
       </div>
-      <div class="item" style="background-image: url(imgs/6.jpg);">
+      <div class="item" style="background-image: url(imgs/1.jpg);">
         <div class="content">
           <div class="name">Serviços</div>
           <div class="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, in perspiciatis explicabo fugit, delectus quam non tenetur temporibus dolore commodi sed dolor nesciunt doloremque et facere ut aspernatur, ipsam quas!</div>
