08fd3fb6323c0419a4ea16ba980521274ae0674e
lippekstro <fellipecastro13@hotmail.com>
2023-04-24 17:55:22 -0300

    js desativado

--- js/botao_agendamento.js
+++ js/botao_agendamento.js
@@ -1,6 +1,7 @@
-function confirmarDelete(event, obj) {
+/* function confirmarDelete(event, obj) {
     event.preventDefault();
     if (confirm("Tem certeza que deseja realizar essa operacao?")) {
+        console.log(obj.href);
         window.location.href = obj.href;
     }
-}
+} */
